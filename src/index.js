8df7903e4db29021032a51e95e2a741e003d7b57
Rendrikson de Oliveira Soares <rendrikson.soares@codevasf.gov.br>
2022-03-09 17:17:27 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const routes = require('./routes');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan())
+
+app.use(routes);
+
+app.listen(3000);
